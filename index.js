20e1e3166befa715dfa83492359302a2ba383cad
JesielJeandc <jesieljeandelacruz25@gmail.com>
2024-06-08 10:25:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ btnDislike2.addEventListener("click", countDislike2)
 const submit = document.getElementById("submit")
 const comment = document.getElementById("comment")
 const commentbox = document.getElementById("commentbox")
+
 function submitComment(){
    commentbox.textContent += comment.value.toString() + "\n"
+  comment.value=""
 }
 submit.addEventlistener("click",submitComment)
